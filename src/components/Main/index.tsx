f120846d0a59fa8a8fecf8b856d6bf5403163d7b
Vanessa Santana <vanessah4money@gmail.com>
2021-03-24 23:11:57 -0300

    feat: styled the first element and refactor test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,12 @@
-const Main = ()=> <main><h1>React Avançado</h1></main>
+import * as S from './styles';
+
+const Main = ()=> (
+    <S.Wrapper>
+        <S.Logo src="/img/logo.svg" alt="Imagem de um átomo e React Avançado escrito ao lado." />
+        <S.Title>React Avançado</S.Title>
+        <S.Description>TypeScript, ReactJS, NextJS e Styled Components</S.Description>
+        <S.Illustrator src="/img/hero-illustration.svg" alt="Um desenvolvedor de frente ṕara uma tela com código" />
+    </S.Wrapper>
+)
 
 export default Main;
