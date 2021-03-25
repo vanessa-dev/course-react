aff3743b5bb29854e861541bdb69efda0b8f90b4
Vanessa Santana <vanessah4money@gmail.com>
2021-03-24 22:06:15 -0300

    style: create GlobalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../../styles/globals.css'
-import type { AppProps} from 'next/app'
+import type { AppProps} from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Head>
+            <title>React Avançado-BoilerPlate</title>
+            <link rel="shortcut icon" href="" />
+            <link rel="apple-touch-icon" href="" />
+            <meta name="description" content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"/>
+        </Head>
+        <GlobalStyles/>
+        <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
