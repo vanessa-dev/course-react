c4957839da47a973319829407394ee21afd721a2
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-09-28 19:46:58 -0300

    class(Boilerplate): Usando Storybook essentials e Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -11,9 +11,3 @@ export default {
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
-Basic.args = {
-    title: 'title basic',
-    description: 'description basic'
-}
-
-export const Default: Story = (args) => <Main {...args} />
