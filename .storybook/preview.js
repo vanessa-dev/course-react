99f08fb8c8daa4b7962ef3f6ab757e408eff96d7
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-09-28 18:00:33 -0300

    class(Boilerplate):  Migrando storybook para versao 6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
+import GlobalStyles from '../src/styles/global'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+];
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,18 +1,19 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react'
 import Main from '.'
 
 export default {
     title: 'Main',
     component: Main,
-    decorators: [withKnobs]
+    args: {
+        title: 'title default',
+        description: 'description default'
+    }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+    title: 'title basic',
+    description: 'description basic'
 }
 
-export const Basic = () => (
-    <Main
-        title={text('Title', 'React avançado')}
-        description={text(
-            'Description',
-            'TypeScript, ReactJS, NextJS e Styled Components'
-        )}
-    />
-)
+export const Default: Story = (args) => <Main {...args} />
