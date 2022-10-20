744fbde679db66f9947f9866741f7537e79e9c33
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-10-20 17:32:27 -0300

    class(grapql/front): Criando o GraphQLClient

---
+++ src/graphql/client.ts
@@ -0,0 +1,6 @@
+import { GraphQLClient } from 'graphql-request'
+
+// ... or create a GraphQL client instance to send requests
+const client = new GraphQLClient('http://localhost:1337/graphql')
+
+export default client
